88bff8dc60c291091474f4c44d8be677dbe74938
Thanh Le <thanh@technocreatives.com>
2020-03-31 14:27:51 +0200

    Just resue code

--- src/commands/create.rs
+++ src/commands/create.rs
@@ -1,46 +1,14 @@
-use structopt::StructOpt;
-
+use super::create_discussion::*;
+use super::create_team::*;
 use anyhow::Result;
-
-use super::common;
-use crate::github;
-
-use crate::github::{CreateTeamResponse, Unauthorized};
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 pub enum CreateArgs {
     #[structopt(name = "team")]
-    Team(TeamArgs),
+    Team(CreateTeamArgs),
     #[structopt(name = "discussion")]
-    Discussion(DiscussionArgs),
-}
-
-#[derive(Debug, StructOpt)]
-pub struct TeamArgs {
-    #[structopt(long, short, default_value = "divvun")]
-    pub organisation: String,
-    #[structopt(long, short)]
-    pub team_name: String,
-    #[structopt(long, short)]
-    pub description: Option<String>,
-    #[structopt(long, short)]
-    pub secret: bool,
-    #[structopt(long, short)]
-    pub members: Vec<String>,
-}
-
-#[derive(Debug, StructOpt)]
-pub struct DiscussionArgs {
-    #[structopt(long, short, default_value = "divvun")]
-    pub organisation: String,
-    #[structopt(long, short)]
-    pub team_slug: String,
-    #[structopt(long, short)]
-    pub header: String,
-    #[structopt(long, short)]
-    pub body: String,
-    #[structopt(long, short)]
-    pub private: bool,
+    Discussion(CreateDiscussionArgs),
 }
 
 impl CreateArgs {
@@ -49,75 +17,5 @@ impl CreateArgs {
             CreateArgs::Discussion(args) => args.create_discusstion(),
             CreateArgs::Team(args) => args.create_team(),
         }
-    }
-}
-
-impl TeamArgs {
-    pub fn create_team(&self) -> Result<()> {
-        let user_token = common::get_user_token()?;
-
-        match create_team(self, &user_token) {
-            Ok(r) => println!(
-            "You created a team named: {} successfully with id: {} and link : {}",
-            self.team_name, r.id, r.html_url
-        ),
-            Err(e) => println!("Failed to create team named: {} because of {}\n. Please notice that you need to add users to your organisation before adding them to a team.", self.team_name, e)
-        }
-
-        Ok(())
-    }
-}
-
-fn create_team(args: &TeamArgs, token: &str) -> Result<CreateTeamResponse> {
-    let empty = &"".to_string();
-    let des: &str = args.description.as_ref().unwrap_or(empty);
-    let members: Vec<String> = args.members.iter().map(|s| s.to_string()).collect();
-    match github::create_team(
-        &args.organisation,
-        &args.team_name,
-        des,
-        members,
-        args.secret,
-        token,
-    ) {
-        Ok(response) => Ok(response),
-        Err(e) => {
-            if e.downcast_ref::<Unauthorized>().is_some() {
-                anyhow::bail!("User token invalid. Run dadmin init with a valid token");
-            }
-            Err(e)
-        }
-    }
-}
-
-impl DiscussionArgs {
-    pub fn create_discusstion(&self) -> Result<()> {
-        let token = common::get_user_token()?;
-
-        match github::create_discusstion(
-            &self.organisation,
-            &self.team_slug,
-            &self.header,
-            &self.body,
-            self.private,
-            &token,
-        ) {
-            Ok(r) => println!(
-                "You created a team discussion for team {} at {}",
-                self.team_slug, r.html_url
-            ),
-            Err(e) => {
-                if e.downcast_ref::<Unauthorized>().is_some() {
-                    anyhow::bail!("User token invalid. Run dadmin init with a valid token");
-                } else {
-                    println!(
-                        "Failed to create a disscusion for team {} because of {}",
-                        self.team_slug, e
-                    );
-                }
-            }
-        }
-
-        Ok(())
     }
 }
