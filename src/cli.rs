89dda6debc4b95ee31e41308a218ff3fa1fea265
Thanh Le <thanh@technocreatives.com>
2020-04-01 10:01:05 +0200

    Finish create command

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,6 @@
 use crate::commands::{
-    AddUsersArgs, CloneArgs, CreateArgs, CreateBranchArgs, CreateDiscussionArgs, CreateTeamArgs,
-    DefaultBranchArgs, InitArgs, ListRepoArgs, ProtectedBranchArgs, RemoveUsersArgs,
-    SetTeamPermissionArgs,
+    AddUsersArgs, CloneArgs, CreateArgs, DefaultBranchArgs, InitArgs, ListRepoArgs,
+    ProtectedBranchArgs, RemoveUsersArgs, SetTeamPermissionArgs,
 };
 use structopt::StructOpt;
 
@@ -20,20 +19,14 @@ pub enum Commands {
     ListRepos(ListRepoArgs),
     #[structopt(name = "cl", aliases = &["clone"])]
     CloneRepos(CloneArgs),
-    #[structopt(name = "cb", aliases = &["create-branch"])]
-    CreateBranch(CreateBranchArgs),
     #[structopt(name = "db", aliases = &["default-branch"])]
     DefaultBranch(DefaultBranchArgs),
     #[structopt(name = "pb", aliases = &["protected-branch"])]
     ProtectedBranch(ProtectedBranchArgs),
-    #[structopt(name = "ct", aliases = &["create-team"])]
-    CreateTeam(CreateTeamArgs),
     #[structopt(name = "au", aliases = &["add-users"])]
     AddUsers(AddUsersArgs),
     #[structopt(name = "ru", aliases = &["remove-users"])]
     RemoveUsers(RemoveUsersArgs),
-    #[structopt(name = "cd", aliases = &["create-discussion"])]
-    CreateDiscussion(CreateDiscussionArgs),
     #[structopt(name = "sp", aliases = &["set-permission"])]
     SetTeamPermission(SetTeamPermissionArgs),
     #[structopt(name = "sc", aliases = &["show-config"])]
--- src/commands/create.rs
+++ src/commands/create.rs
@@ -1,3 +1,4 @@
+use super::create_branch::*;
 use super::create_discussion::*;
 use super::create_team::*;
 use anyhow::Result;
@@ -9,6 +10,8 @@ pub enum CreateArgs {
     Team(CreateTeamArgs),
     #[structopt(name = "discussion")]
     Discussion(CreateDiscussionArgs),
+    #[structopt(name = "branch")]
+    Branch(CreateBranchArgs),
 }
 
 impl CreateArgs {
@@ -16,6 +19,7 @@ impl CreateArgs {
         match self {
             CreateArgs::Discussion(args) => args.create_discusstion(),
             CreateArgs::Team(args) => args.create_team(),
+            CreateArgs::Branch(args) => args.create_branch(),
         }
     }
 }
