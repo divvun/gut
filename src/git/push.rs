94f648d3ad475d5d491a37e35c2d93dbd3279721
Thanh Le <thanh@technocreatives.com>
2020-04-02 12:55:01 +0200

    Use user's token when pushing new branch

--- src/git/push.rs
+++ src/git/push.rs
@@ -1,14 +1,27 @@
+use super::models::GitCredential;
 use git2::{Error, Repository};
 use git2_credentials::ui4dialoguer::CredentialUI4Dialoguer;
 use git2_credentials::CredentialHandler;
+use git2_credentials::CredentialUI;
 
-pub fn push_branch(repo: &Repository, branch: &str, remote_name: &str) -> Result<(), Error> {
+pub fn push_branch(
+    repo: &Repository,
+    branch: &str,
+    remote_name: &str,
+    cred: Option<GitCredential>,
+) -> Result<(), Error> {
     let mut origin = repo.find_remote(remote_name)?;
 
     let mut cb = git2::RemoteCallbacks::new();
     let git_config = git2::Config::open_default()?;
+
+    let credential_ui: Box<dyn CredentialUI> = match cred {
+        Some(gc) => Box::new(gc),
+        _ => Box::new(CredentialUI4Dialoguer {}),
+    };
+
     // Prepare callbacks.
-    let mut ch = CredentialHandler::new_with_ui(git_config, Box::new(CredentialUI4Dialoguer {}));
+    let mut ch = CredentialHandler::new_with_ui(git_config, credential_ui);
 
     cb.credentials(move |url, username, allowed| ch.try_next_credential(url, username, allowed));
 
