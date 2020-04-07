cf472c1706ce0b779df9f63d11842124690e0be3
Thanh Le <thanh@technocreatives.com>
2020-04-07 09:07:47 +0200

    temp

--- src/commands/push.rs
+++ src/commands/push.rs
@@ -1,7 +1,14 @@
+use super::common;
+use crate::convert::try_from_one;
+use crate::github::RemoteRepo;
+use crate::user::User;
+
 use anyhow::Result;
 
 use crate::filter::Filter;
 use structopt::StructOpt;
+use crate::git::push;
+use git2::Repository;
 
 #[derive(Debug, StructOpt)]
 pub struct PushArgs {
@@ -15,7 +22,22 @@ pub struct PushArgs {
 
 impl PushArgs {
     pub fn run(&self) -> Result<()> {
-        println!("push branch {:?}", self);
+        log::debug!("push branch {:?}", self);
+
+        let user = common::user()?;
+
+        let filtered_repos =
+            common::query_and_filter_repositories(&self.organisation, &self.regex, &user.token)?;
+
         Ok(())
     }
+}
+
+fn push_branch(
+    repo: &Repository,
+    branch: &str,
+    user: &User,
+) -> Result<()> {
+
+    Ok(())
 }
