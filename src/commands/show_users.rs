96cdcf039d2f2e6f955c30c2b58be9791f0f93fb
Thanh Le <thanh@technocreatives.com>
2020-05-18 10:45:55 +0200

    Use GraphQl to query all users

--- src/commands/show_users.rs
+++ src/commands/show_users.rs
@@ -1,6 +1,6 @@
 use super::common;
 use crate::github;
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -8,21 +8,21 @@ use structopt::StructOpt;
 pub struct ShowUsersArgs {
     #[structopt(long, short, default_value = "divvun")]
     pub organisation: String,
-    #[structopt(long, short, default_value = "all", parse(try_from_str = parse_role))]
-    /// Filter members returned by their role.
-    ///
-    /// Can be one of:
-    /// * all - All members of the organization, regardless of role.
-    /// * admin - Organization owners.
-    /// * member - Non-owner organization members.
-    pub role: String,
+    //#[structopt(long, short, default_value = "all", parse(try_from_str = parse_role))]
+    // Filter members returned by their role.
+    //
+    // Can be one of:
+    // * all - All members of the organization, regardless of role.
+    // * admin - Organization owners.
+    // * member - Non-owner organization members.
+    //pub role: String,
 }
 
 impl ShowUsersArgs {
     pub fn run(&self) -> Result<()> {
         let user_token = common::user_token()?;
 
-        let result = github::get_org_members(&self.organisation, &self.role, &user_token);
+        let result = github::get_org_members(&self.organisation, &user_token);
 
         match result {
             Ok(users) => print_results(&users),
@@ -40,12 +40,12 @@ fn print_results(users: &[github::OrgMember]) {
     }
 }
 
-fn parse_role(src: &str) -> Result<String> {
-    let roles = ["all", "admin", "member"];
-    let src = src.to_lowercase();
-    if roles.contains(&src.as_str()) {
-        return Ok(src);
-    }
+//fn parse_role(src: &str) -> Result<String> {
+//let roles = ["all", "admin", "member"];
+//let src = src.to_lowercase();
+//if roles.contains(&src.as_str()) {
+//return Ok(src);
+//}
 
-    Err(anyhow!("role must be one of {:?}", roles))
-}
+//Err(anyhow!("role must be one of {:?}", roles))
+//}
