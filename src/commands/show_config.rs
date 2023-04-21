a1a78c24356e22b4ca459569f1cca38d77ed67e0
Flammie A Pirinen <flammie@iki.fi>
2023-04-21 11:59:07 +0200

    show new config values

--- src/commands/show_config.rs
+++ src/commands/show_config.rs
@@ -4,11 +4,19 @@ use crate::config::Config;
 pub fn show_config() -> anyhow::Result<()> {
     let user = common::user()?;
     let root = Config::root()?;
+    let organisation = match common::organisation(None) {
+        Ok(s) => s,
+        Err(_) => "(no default org)".to_string(),
+    };
+    let use_https = common::use_https()?;
 
     println!(
         "Username: {}\nGithub token: {}\nRoot directory: {}",
         user.username, user.token, root
     );
+    println!(
+        "Default org: {}\nHttps? {}",
+        organisation, use_https);
 
     Ok(())
 }
