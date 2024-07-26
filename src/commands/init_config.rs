2ae2eb04aac36f7bc76c51777b5e4b0b939404fe
Anders Lorentsen <anders.lorentsen@gmail.com>
2024-07-26 16:39:39 +0200

    fixed `init -r` default value. also bump deps.
    When running `gut init` without giving it a root directory with
    -r, gut would crash with a message saying something like
    "invalid root folder 'RootDirectory::default()'". This is now fixed,
    so that not giving -r to `gut init` will select the home directory
    correctly.
    
    This commit also bumps dependencies. Note that one dependency had
    to be kept to not-the-newest version, as I kept running into
    dependency issues.

--- src/commands/init_config.rs
+++ src/commands/init_config.rs
@@ -1,18 +1,43 @@
+use std::path::PathBuf;
 use crate::cli::Args as CommonArgs;
-use super::models::RootDirectory;
 use crate::config::Config;
 use crate::github;
 use crate::user::User;
 use clap::Parser;
 
+fn validate_root(root: &str) -> Result<PathBuf, String> {
+    let path = PathBuf::from(root);
+
+    if path.is_relative() {
+        return Err(format!("{root} is not an absolute path. Root must be an absolute path"));
+    }
+
+    if path.exists() {
+        if path.is_dir() {
+            Ok(path)
+        } else {
+            Err(format!("{root} is a file. Root directory cannot be a file"))
+        }
+    } else {
+        std::fs::create_dir_all(&path)
+            .map_err(|source| format!("Cannot create root directory for path: {path:?} ({source})"))
+            .map(|_| path)
+    }
+}
+
 #[derive(Debug, Parser)]
 /// Init configuration data
 pub struct InitArgs {
-    #[arg(long, short, default_value = "RootDirectory::default()")]
+    #[arg(
+        long,
+        short,
+        default_value = dirs::home_dir().unwrap().into_os_string(),
+        value_parser = clap::builder::ValueParser::new(validate_root),
+    )]
     /// The root directory. This must be an absolute path.
     ///
     /// All repositories will be cloned under this directory
-    pub root: RootDirectory,
+    pub root: PathBuf,
     #[arg(short, long)]
     /// Github token. Gut needs github token to access your github data
     pub token: String,
@@ -35,7 +60,7 @@ impl InitArgs {
             };
         user.save_user()?;
         let config = Config::new(
-            self.root.path.clone(),
+            self.root.to_str().unwrap().to_string(),
             self.organisation.clone(),
             self.use_https,
         );
