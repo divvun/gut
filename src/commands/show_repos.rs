0df65e51cdea5cff00518074ed25be236e546bbb
Dylan Hand <dylan.hand@gmail.com>
2026-01-20 11:12:40 +0100

    Refactor to reduce duplication, add spinner when pinging github api

--- src/commands/show_repos.rs
+++ src/commands/show_repos.rs
@@ -1,11 +1,12 @@
 use super::common;
-
 use crate::filter::Filter;
 use crate::github::{self, RemoteRepo};
 use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
 use prettytable::{Table, format, row};
 use rayon::prelude::*;
 use std::path::Path;
+use std::time::Duration;
 
 #[derive(Debug, Parser)]
 /// Show all repositories that match a pattern
@@ -34,77 +35,77 @@ impl ShowReposArgs {
         let user_token = common::user_token()?;
         let root = common::root()?;
 
-        if self.all_orgs {
-            self.show_all_orgs(&user_token, &root)
+        let organizations = if self.all_orgs {
+            let orgs = common::get_all_organizations()?;
+            if orgs.is_empty() {
+                println!("No organizations found in root directory");
+                return Ok(());
+            }
+            orgs
         } else {
-            let organisation = common::organisation(self.organisation.as_deref())?;
-            self.show_single_org(&organisation, &user_token, &root)
-        }
-    }
-
-    fn show_single_org(
-        &self,
-        organisation: &str,
-        user_token: &str,
-        root: &str,
-    ) -> anyhow::Result<()> {
-        let filtered_repos =
-            common::query_and_filter_repositories(organisation, self.regex.as_ref(), user_token)?;
+            vec![common::organisation(self.organisation.as_deref())?]
+        };
 
         if self.json {
-            print_json(&filtered_repos)?;
-        } else {
-            print_table(
-                &filtered_repos,
-                organisation,
-                root,
-                user_token,
-                self.default_branch,
-            )?;
-        }
-
-        Ok(())
-    }
-
-    fn show_all_orgs(&self, user_token: &str, root: &str) -> anyhow::Result<()> {
-        let organizations = common::get_all_organizations()?;
-
-        if organizations.is_empty() {
-            println!("No organizations found in root directory");
-            return Ok(());
-        }
-
-        if self.json {
-            // For JSON mode, collect all repos from all orgs
             let mut all_repos = Vec::new();
             for org in &organizations {
-                if let Ok(repos) =
-                    common::query_and_filter_repositories(org, self.regex.as_ref(), user_token)
-                {
+                if let Ok(repos) = self.show_org(org, &user_token, &root, true) {
                     all_repos.extend(repos);
                 }
             }
             print_json(&all_repos)?;
         } else {
-            // For table mode, print a table for each org
             for org in &organizations {
-                println!("\n=== {} ===", org);
-                match common::query_and_filter_repositories(org, self.regex.as_ref(), user_token) {
-                    Ok(repos) => {
-                        if repos.is_empty() {
-                            println!("No repositories match the pattern");
-                        } else {
-                            print_table(&repos, org, root, user_token, self.default_branch)?;
-                        }
-                    }
-                    Err(e) => {
-                        println!("Could not fetch repositories for {}: {:?}", org, e);
-                    }
-                }
+                let _ = self.show_org(org, &user_token, &root, false);
             }
         }
 
         Ok(())
+    }
+
+    fn show_org(
+        &self,
+        organisation: &str,
+        user_token: &str,
+        root: &str,
+        json_mode: bool,
+    ) -> anyhow::Result<Vec<RemoteRepo>> {
+        println!("\n=== {} ===", organisation);
+
+        let spinner = ProgressBar::new_spinner();
+        spinner.set_style(
+            ProgressStyle::default_spinner()
+                .template("{spinner:.green} {msg}")
+                .unwrap(),
+        );
+        spinner.set_message("Querying GitHub...");
+        spinner.enable_steady_tick(Duration::from_millis(100));
+
+        let repos = match common::query_and_filter_repositories(
+            organisation,
+            self.regex.as_ref(),
+            user_token,
+        ) {
+            Ok(repos) => {
+                spinner.finish_and_clear();
+                repos
+            }
+            Err(e) => {
+                spinner.finish_and_clear();
+                println!("Could not fetch repositories for {}: {:?}", organisation, e);
+                return Ok(Vec::new());
+            }
+        };
+
+        if !json_mode {
+            if repos.is_empty() {
+                println!("No repositories match the pattern");
+            } else {
+                print_table(&repos, organisation, root, user_token, self.default_branch)?;
+            }
+        }
+
+        Ok(repos)
     }
 }
 
